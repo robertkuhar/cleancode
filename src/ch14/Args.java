8918d77c3d2cc0e78a980f81ce253cb5bba70683
robert.kuhar <robert.kuhar@climate.com>
2015-08-23 23:39:02 -0700

    A couple of comments

--- src/ch14/Args.java
+++ src/ch14/Args.java
@@ -17,31 +17,34 @@ public class Args {
 
   private void parseSchema(String schema) throws ArgsException {
     for (String element : schema.split(","))
-      if (element.length() > 0)
+      if (element.length() > 0) {
         parseSchemaElement(element.trim());
+      }
   }
 
   private void parseSchemaElement(String element) throws ArgsException {
     char elementId = element.charAt(0);
     String elementTail = element.substring(1);
     validateSchemaElementId(elementId);
-    if (elementTail.length() == 0)
+    if (elementTail.length() == 0) {
       marshalers.put(elementId, new BooleanArgumentMarshaler());
-    else if (elementTail.equals("*"))
+    } else if (elementTail.equals("*")) {
       marshalers.put(elementId, new StringArgumentMarshaler());
-    else if (elementTail.equals("#"))
+    } else if (elementTail.equals("#")) {
       marshalers.put(elementId, new IntegerArgumentMarshaler());
-    else if (elementTail.equals("##"))
+    } else if (elementTail.equals("##")) {
       marshalers.put(elementId, new DoubleArgumentMarshaler());
-    else if (elementTail.equals("[*]"))
+    } else if (elementTail.equals("[*]")) {
       marshalers.put(elementId, new StringArrayArgumentMarshaler());
-    else
+    } else {
       throw new ArgsException(INVALID_ARGUMENT_FORMAT, elementId, elementTail);
+    }
   }
 
   private void validateSchemaElementId(char elementId) throws ArgsException {
-    if (!Character.isLetter(elementId))
+    if (!Character.isLetter(elementId)) {
       throw new ArgsException(INVALID_ARGUMENT_NAME, elementId, null);
+    }
   }
 
   private void parseArgumentStrings(List<String> argsList) throws ArgsException {
@@ -57,8 +60,9 @@ public class Args {
   }
 
   private void parseArgumentCharacters(String argChars) throws ArgsException {
-    for (int i = 0; i < argChars.length(); i++)
+    for (int i = 0; i < argChars.length(); i++) {
       parseArgumentCharacter(argChars.charAt(i));
+    }
   }
 
   private void parseArgumentCharacter(char argChar) throws ArgsException {
--- src/ch14/Main.java
+++ src/ch14/Main.java
@@ -3,6 +3,10 @@ package ch14;
 public class Main {
   public static void main(String[] args) {
     try {
+      /*
+       * ["-l","-p","8080","-d","~/work"] produces logging:true, port:8080,
+       * directory:~/work
+       */
       Args arg = new Args("l,p#,d*", args);
       boolean logging = arg.getBoolean('l');
       int port = arg.getInt('p');
@@ -14,5 +18,6 @@ public class Main {
   }
 
   private static void executeApplication(boolean logging, int port, String directory) {
+    System.out.println("logging:" + logging + ", port: " + port + ", directory " + directory);
   }
 }
