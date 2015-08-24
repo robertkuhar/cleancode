9a912b9006fae1f2a4067c10bed8813d94512b89
robert.kuhar <robert.kuhar@climate.com>
2015-08-24 08:14:18 -0700

    Comments and stuff

--- src/ch14/ArgumentMarshaler.java
+++ src/ch14/ArgumentMarshaler.java
@@ -8,5 +8,9 @@ import java.util.Iterator;
  */
 
 public interface ArgumentMarshaler {
+  /*
+   * FIXME: This stinks, no? I am changing the state of my input argument by
+   * this action. Maybe not here on the interface, but on the implementations.
+   */
   void set(Iterator<String> currentArgument) throws ArgsException;
 }
